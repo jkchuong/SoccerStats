187fa08d9c85d8af3123a6187dcd2be84f7c3b7f
jkchuong <jkchuong@gmail.com>
2020-12-28 12:53:22 +0000

    Added property reader to read csv filie

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -17,9 +17,19 @@ namespace SoccerStats
             var files = directory.GetFiles("*.txt");
 
             // Path combines two strings into a directory path => adds the \ into the string
-            var fileName = Path.Combine(directory.FullName, "data.txt");
-            var file = new FileInfo(fileName);
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadSoccerResults(fileName);
 
+            /*
+            var fileContents = ReadFile(fileName);
+
+            // get each line in the file without empty lines that are split by the \r and \n
+            string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in fileLines)
+            {
+                Console.WriteLine(line);
+            }
+            
             // check if file exists, then sets the text if file does.
             if (file.Exists)
             {
@@ -33,6 +43,73 @@ namespace SoccerStats
                 }
 
             }
+            */
+
+            Console.ReadKey();
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static List<GameResult> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<GameResult>();
+            using(var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                reader.ReadLine();
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var gameResult = new GameResult(); 
+                    string[] values = line.Split(',');
+
+                    DateTime gameDate;
+                    if (DateTime.TryParse(values[0], out gameDate))
+                    {
+                        gameResult.GameDate = gameDate;
+                    }
+
+                    gameResult.TeamName = values[1];
+
+                    HomeOrAway homeOrAway;
+                    if (Enum.TryParse(values[2], out homeOrAway))
+                    {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
+
+                    int parseInt;
+                    if (int.TryParse(values[3], out parseInt))
+                    {
+                        gameResult.Goals = parseInt;
+                    }
+                    if (int.TryParse(values[4], out parseInt))
+                    {
+                        gameResult.GoalAttempts = parseInt;
+                    }
+                    if (int.TryParse(values[5], out parseInt))
+                    {
+                        gameResult.ShotsOnGoal = parseInt;
+                    }
+                    if (int.TryParse(values[6], out parseInt))
+                    {
+                        gameResult.ShotsOffGoal = parseInt;
+                    }
+
+                    double possessionPercent;
+                    if (double.TryParse(values[7], out possessionPercent))
+                    {
+                        gameResult.PossessionPercent = possessionPercent;
+                    }
+                    
+                    soccerResults.Add(gameResult);
+                }
+            }
+            return soccerResults;
         }
     }
 }
